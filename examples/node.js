149ae0cc2f1fe1c9dad12e732465988a483f8247
Aytekin Yaliz <aytekinyaliz@gmail.com>
2019-04-05 14:52:12 +0100

    Update node.js

--- examples/node.js
+++ examples/node.js
@@ -1,16 +1,19 @@
 // utils
-const util = require('util');
-client.get = util.promisify(client.get);
+const { promisify } = require('util');
+client.get = promisify(client.get);
 const blogs = await client.get(123);
+
+
+// Mongo
 
 
 // Redis
 const redis = require('redis');
 const client = redis.createClient('redis://127.0.0.1:6379');
-client.flushall()
+client.flushall();
 
 client.set('color', 'red');
 client.get('color', console.log);    // null, 'red'
 client.hset('hash', 'field', 'value');
-client.hget('hash', 'field', console.log) // null, 'value'
+client.hget('hash', 'field', console.log); // null, 'value'
 client.set('color', 'red', 'EX', 5); // stores in the cache for 5 sec
